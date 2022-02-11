443482c03e48c1b332dee266c8ac0a2aa8ff392a
Igor Martins <igor.martins.silverio@gmail.com>
2022-02-11 17:18:42 -0300

    initial commit

---
+++ src/main/kotlin/com/igormartinssilverio/aluraexpenses/AluraExpensesApplication.kt
@@ -0,0 +1,11 @@
+package com.igormartinssilverio.aluraexpenses
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AluraExpensesApplication
+
+fun main(args: Array<String>) {
+    runApplication<AluraExpensesApplication>(*args)
+}
---
+++ src/test/kotlin/com/igormartinssilverio/aluraexpenses/AluraExpensesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.igormartinssilverio.aluraexpenses
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AluraExpensesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
